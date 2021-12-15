cd7f41eae9e93f8ff9fe4dbfca1c61ca62d44fce
Zach Harris <zharris654@gmail.com>
2021-12-15 14:43:51 -0500

    Added functionality for first and last name searches

--- csv_filter.bash
+++ csv_filter.bash
@@ -1,16 +1,54 @@
 #!/bin/bash
 #
-# src - Source code for the solution to the CSV Filter challenge
+# csv_filter - Source code for the solution to the CSV Filter challenge
 #
 # Zach Harris
 #
 # 12/15/21
 #
-# The purpose of this program is to parse a CSV file containing a list of last and first names, as well as a date of birth, based
+# The purpose of this program is to parse a CSV file containing a list of first and last names, as well as a date of birth, based
 # on user input
 #
 
-file = $1
+file=$1
 
 read -p "Please select an option to filter by: " filter
-echo "You have chosen: $filter" 
+read -p "Please enter the name or birth year you are searching for: " search
+echo -e "\nFiltering by $filter..."
+echo -e "Searching for $search...\n"
+
+while read line; do
+	case $filter in
+
+		first_name)
+
+			test=$(echo $line | awk -F"," '{print $1}')
+		
+			if [ $test = $search ]
+			then
+				echo $line
+			fi
+			;;
+		
+		last_name)
+			
+			test=$(echo $line | awk -F"," '{print $2}')
+			
+			if [ $test = $search ]
+			then
+				echo $line
+			fi
+			;;
+
+		birth_year)
+			
+			test=$(echo $line | awk -F"," '{print $3}')
+			year="{test:0:4}"
+			
+			if [ $test = $search ]
+			then
+				echo $line
+			fi
+			;;
+		esac
+done < $file
